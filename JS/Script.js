bbf3e6b46b094abc8342985297f39cbe597aaf6e
ETPL2023 <126471338+ETPL2023@users.noreply.github.com>
2023-03-02 16:02:09 +0530

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -84,7 +84,7 @@ function Complain(){
             const form = document.forms['ETPL Form']
 
             fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => alert("Thank you! your form is submitted successfully." ))
+            .then(response => alert("Thank you! your complaint is received. Please Save your complaint Id for the future." ))
             .then(() => {  window.location.reload(); })
             .catch(error => console.error('Error!', error.message))
 
